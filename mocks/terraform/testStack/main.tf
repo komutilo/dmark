d71cbbfaf0b89c42fd80c51ef0a1ce109ce5d99d
Bruno Araujo <bruno@komutilo.com>
2024-06-06 14:36:35 -0300

    add tofu as runner (#11)
    * fix: setting.json for terraform files
    
    * chore: add .terraform folders to .gitignore
    
    * chore: set version as 1.1.0-alpha1
    
    * feat: added runner field in schema validation and runner use in cmd
    
    * fix: fixes after manual tests
    
    * chore: types generated
    
    * fix: reduce package size
    
    * chore: adjusts to deploy version 1.1.0

--- mocks/terraform/testStack/main.tf
+++ mocks/terraform/testStack/main.tf
@@ -10,14 +10,21 @@ provider "aws" {
   region = "sa-east-1"
 }
 
+variable "stage" {
+  type        = string
+  description = "The current stage of the infrastructure."
+}
+
 variable "bucket_prefix" {
-  type = string
+  type        = string
+  description = "An input of a specific bucket name prefix."
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.bucket_prefix}-bucket"
+  bucket        = "${var.stage}-${var.bucket_prefix}-bucket"
+  force_destroy = true
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.bucket.bucket
+  value = aws_s3_bucket.bucket.id
 }
